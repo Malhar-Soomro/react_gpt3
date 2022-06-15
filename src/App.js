c5c8e4a3d9a62a3f248e8c10bf2bcc47a950ca53
Malhar-Soomro <malharsoomro2@gmail.com>
2022-06-15 17:32:23 +0500

    setup file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Article, Brand, CTA, Feature, Navbar } from "./components"
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./containers"
 
 function App() {
   return (
     <div className="App">
-
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+      Hey guys
     </div>
   );
 }
